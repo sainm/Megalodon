dd0f68495dee8f9c218d5eaaee235ece84d65644
sainm <sainmhan@gmail.com>
2020-11-23 21:51:14 +0800

    update Options.cs Listener.cs

--- Megalodon.Driver/Program.cs
+++ Megalodon.Driver/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using CommandLine;
 
 namespace Megalodon.Driver
 {
@@ -6,7 +8,27 @@ namespace Megalodon.Driver
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(RunOptions)
+                .WithNotParsed(HandleParseError);
+            
+           
+        }
+
+        private static void HandleParseError(IEnumerable<Error> obj)
+        {
+           // var listener = new Listener(port);
+        }
+
+        private static void RunOptions(Options options)
+        {
+            if (options.Port.HasValue)
+            {
+                var listener = new Listener(options.Port.Value);
+                listener.Start();
+            }
+            throw new NotImplementedException();
         }
     }
 }
